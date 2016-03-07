b9d9b39e1089a3ebd7ed7493492daddc641f1427
Jon Hammond <jonh1016@gmail.com>
2016-03-07 11:08:12 -0700

    Finished npm install after 45 years, now all node modules are installed. Yay.

--- src/server/routes/encryption.js
+++ src/server/routes/encryption.js
@@ -1,8 +1,15 @@
 var express = require('express');
 var router = express.Router();
+var md5 = require('md5');
 
 router.get('/', function(req, res, next) {
   res.render('index', { title: 'Encryption', result: undefined });
+});
+
+router.get('/md5', function(req, res, next) {
+  var pass = 'simple-password';
+  var result = md5(pass);
+  res.render('index', { title: 'Encryption', result: result });
 });
 
 module.exports = router;
